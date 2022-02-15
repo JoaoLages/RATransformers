f4e9d36d959dd0aa4775c866886b448b73b9e7d1
JoaoLages <joaop.glages@gmail.com>
2022-02-15 11:12:11 +0000

    update requirements

--- setup.py
+++ setup.py
@@ -1,51 +1,24 @@
 from setuptools import setup, find_packages
-import pathlib
 
 
-here = pathlib.Path(__file__).parent.resolve()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
-# Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+with open('requirements.txt', encoding='utf-8') as f:
+    required = f.read().splitlines()
 
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
 setup(
     name='ratransformers',
     version='0.0.0',
-    # description='A sample Python project',
+    description='RATransformer - make a transformer model learn implicit relations passed in the input',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/JoaoLages/RATransformers',
     author='Joao Lages',
     author_email='joaop.glages@gmail.com',
-
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    packages=find_packages(where='src'),
-    install_requires=[
-        "transformers ~= 4.6",
-        "numpy ~= 1.19",
-        "PyYAML~=5.4"
-    ],
+    license='MIT',
+    packages=find_packages('src'),
     package_dir={'': 'src'},
-    python_requires='>=3.6, <4',
-    dependency_links=[
-        "https://download.pytorch.org/whl/torch_stable.html"
-    ]
+    install_requires=required
 )
