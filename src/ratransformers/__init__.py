e02e3d748c828b709fbdc5f55f3053a2bb489890
JoaoLages <joaop.glages@gmail.com>
2022-12-12 14:53:31 +0000

    remove pytest from dependencies

--- src/ratransformers/__init__.py
+++ src/ratransformers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from transformers import AutoTokenizer, AutoModel, BertPreTrainedModel, BartPretrainedModel, T5PreTrainedModel, \
     PreTrainedTokenizer, BatchEncoding, GPT2PreTrainedModel, PreTrainedModel, LongT5PreTrainedModel
